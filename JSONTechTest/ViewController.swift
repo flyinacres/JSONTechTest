08b16cd9f54608b2c551d365b21702af9e661b24
Ronald Fischer <rff@ronalds-macbook-pro-2.local>
2015-11-13 22:08:21 -0600

    First tests with AlamoFire and SwiftJSON

--- JSONTechTest/ViewController.swift
+++ JSONTechTest/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Test basic Alamofire functionality
+        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("AlamoFire JSON: \(JSON)")
+                }
+        }
+        
+        // Because of async nature of the calls this attempt at separation doesn't really do anything
+        //print("\n\nAnd now for the SwiftyJSON version\n\n")
+        
+        // SwiftyJSON version
+        Alamofire.request(.GET, "https://httpbin.org/get").validate().responseJSON { response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    print("SwiftyJSON: \(json)")
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
